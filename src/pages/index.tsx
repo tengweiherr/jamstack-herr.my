8b6b444320bfa600b32da739736ab9fbd51f5076
Teng Wei Herr <weiherrteng@gmail.com>
2023-03-12 19:08:44 +0800

    change to SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,14 +21,6 @@ export async function getServerSideProps({ req, res }:GetServerSidePropsContext)
     'public, max-age=31536000, immutable'
   )
 
-  return {
-    props: {},
-  }
-}
-
-// This function gets called at build time
-export async function getStaticProps() {
-
   const projects = await fetchHighlightedProjects()
   const myData = await fetchMyData()
   const exps = await fetchExp()
@@ -41,6 +33,22 @@ export async function getStaticProps() {
     },
   }
 }
+
+// This function gets called at build time
+// export async function getStaticProps() {
+
+//   const projects = await fetchHighlightedProjects()
+//   const myData = await fetchMyData()
+//   const exps = await fetchExp()
+
+//   return {
+//     props: {
+//       projects,
+//       myData,
+//       exps
+//     },
+//   }
+// }
 
 export default function Home({projects,myData,exps}:HomeProps) {
 
