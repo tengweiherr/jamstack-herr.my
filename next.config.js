a697126edfd8b089e64f1ec047d394cbf85d9da5
Teng Wei Herr <weiherrteng@gmail.com>
2023-03-16 01:16:52 +0800

    try set header in next.config.js

--- next.config.js
+++ next.config.js
@@ -25,7 +25,21 @@ const nextConfig = {
     ],
     minimumCacheTTL: 604800
   },
-  transpilePackages: ['gsap']
+  transpilePackages: ['gsap'],
+  async headers() {
+    return [
+      {
+        source: '/:all*(svg|webp)',
+        locale: false,
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=9999999999, immutable',
+          }
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
