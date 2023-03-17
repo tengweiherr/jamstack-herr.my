5ae93398ac89b009b6479d2746d767bc3f70db05
Teng Wei Herr <weiherrteng@gmail.com>
2023-03-17 20:48:06 +0800

    set new build command

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,26 +1,11 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// import type { NextApiRequest, NextApiResponse } from 'next'
-
-// type Data = {
-//   name: string
-// }
-
-// export default function handler(
-//   req: NextApiRequest,
-//   res: NextApiResponse<Data>
-// ) {
-//   res.status(200).json({ name: 'John Doe' })
-// }
-
-
-// pages/api/hello.js
-
 // Next.js Edge API Routes: https://nextjs.org/docs/api-routes/edge-api-routes
+
+import type { NextRequest } from 'next/server'
 
 export const config = {
   runtime: 'edge',
 }
 
-export const errorResponse = () => new Response("Error message", {
-  status: 400,
-});
+export default async function handler(req: NextRequest) {
+  return new Response(JSON.stringify({ name: 'John Doe' }))
+}
