1290806f1ea3fa3035d05750414b3d509f42b97a
Teng Wei Herr <weiherrteng@gmail.com>
2023-03-17 21:06:25 +0800

    add back edge api

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@ const nextConfig = {
   experimental: {
     runtime: 'experimental-edge',
   },
-  swcMinify: true,
   compiler: {
     // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
     styledComponents: {
