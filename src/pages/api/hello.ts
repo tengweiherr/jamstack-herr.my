b4eb784877bafd2422111e642a96a83288e77f54
Teng Wei Herr <weiherrteng@gmail.com>
2023-03-17 20:40:36 +0800

    change on Functions

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -17,14 +17,10 @@
 
 // Next.js Edge API Routes: https://nextjs.org/docs/api-routes/edge-api-routes
 
-// Next.js Edge API Routes: https://nextjs.org/docs/api-routes/edge-api-routes
-
-import type { NextRequest } from 'next/server'
-
 export const config = {
   runtime: 'edge',
 }
 
-export default async function handler(req: NextRequest) {
-  return new Response(JSON.stringify({ name: 'John Doe' }))
-}
+export const errorResponse = () => new Response("Error message", {
+  status: 400,
+});
