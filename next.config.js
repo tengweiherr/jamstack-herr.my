0d3db397d333cbeb40b157c43df8db0d42315bcc
Teng Wei Herr <weiherrteng@gmail.com>
2023-03-17 17:50:30 +0800

    change runtime to experimental-edge

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    runtime: 'edge',
+    runtime: 'experimental-edge',
   },
   compiler: {
     // see https://styled-components.com/docs/tooling#babel-plugin for more info on the options.
