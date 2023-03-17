8c8eaa49090b3b0a74c3b163b924b94b283486cc
Teng Wei Herr <weiherrteng@gmail.com>
2023-03-17 22:55:24 +0800

    runtime

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -2,6 +2,10 @@
 
 import type { NextRequest } from 'next/server'
 
+export const config = {
+    runtime: "edge",
+  };
+  
 export default async function handler(req: NextRequest) {
   return new Response(JSON.stringify({ name: 'John Doe' }))
 }
