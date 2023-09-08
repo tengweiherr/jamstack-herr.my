7be279e2880d0cddb53f8ea945eb094217ec6a51
Teng Wei Herr <weiherrteng@gmail.com>
2023-09-08 23:56:43 +0800

    update path

--- src/utils/api/medium.ts
+++ src/utils/api/medium.ts
@@ -1,7 +1,7 @@
 import { MediumStory } from '../types'
 
 export const fetchAllStories = async (): Promise<MediumStory[] | undefined> => {
-  const res = await fetch(process.env.BASE_FETCH_URL + '/api/medium')
+  const res = await fetch('/api/medium')
   const resInJSON = await res.json()
   return resInJSON?.items
 }
