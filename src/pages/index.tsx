cfb5c0770c243a0dbb86316598613b957332d4fc
Teng Wei Herr <weiherrteng@gmail.com>
2023-09-10 18:07:18 +0800

    remove null value

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -119,7 +119,7 @@ export async function getStaticProps() {
   return {
     props: {
       projects,
-      myData: myData || null,
+      myData,
       myDataParagraphs,
       myDataSkills,
       exps: expsToModify,
