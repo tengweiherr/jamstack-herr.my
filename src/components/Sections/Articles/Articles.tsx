5a7a5b76cc39a9534564c6e1062311fd68a9b08c
Teng Wei Herr <weiherrteng@gmail.com>
2025-04-26 15:40:43 +0800

    update articles

--- src/components/Sections/Articles/Articles.tsx
+++ src/components/Sections/Articles/Articles.tsx
@@ -43,10 +43,22 @@ export const Articles = () => {
         </TextContainer>
         <TechblogCardList mediumStories={[
             {
+              title: "Adaptive Video Streaming With Dash.js In React",
+              subtitle: "A complete guide to build a ABR video player in React starting with a MP4 video",
+              date: "2025-03-27",
+              link: "https://www.smashingmagazine.com/2025/03/adaptive-video-streaming-dashjs-react"
+            },
+            {
               title: "Next.js SSG: Caching Third-Party Assets for Zero Runtime Cost",
               subtitle: "Minimizing Bandwidth Usage by Serving Third-Party Assets Statically in Next.js",
               date: "2024-09-09",
               link: "https://levelup.gitconnected.com/next-js-ssg-caching-third-party-assets-for-zero-runtime-cost-c99de7a36cd3?sk=6ff4f653d63606e0d8ae582891dd1b15"
+            },
+            {
+              title: "How to Identify Web Performance Bottlenecks Using a Browser Profiler",
+              subtitle: "A Step-by-Step Guide to Diagnosing Performance Issues in Web Applications",
+              date: "2024-08-20",
+              link: "https://levelup.gitconnected.com/how-to-identify-web-performance-bottlenecks-using-a-browser-profiler-0f92426d3a6e?sk=fdb075a4993fb9baa1e41af032ad4277"
             },
             {
               title: "Chrome’s Performance Panel 101: What’s Going on in Your JavaScript Main Thread",
@@ -59,6 +71,12 @@ export const Articles = () => {
               subtitle: "Learn the underlying concepts and how to plot charts with D3.js and React",
               date: "2024-01-29",
               link: "https://levelup.gitconnected.com/animated-bar-chart-with-d3-js-and-react-54779f19d333?sk=1c0f3bf92b55fe5486d3f066508bb83a"
+            },
+            {
+              title: "Next.js Middleware: Server-side A/B Testing for Enhanced Performance",
+              subtitle: "Server-side Feature Flagging via Firebase Remote Config Using Next.js Middleware — With Optimal Performance",
+              date: "2023-08-27",
+              link: "https://levelup.gitconnected.com/next-js-middleware-server-side-a-b-testing-for-enhanced-performance-f13ed0aa0b40?sk=d5375beaa554e3122818c1d3a6eac53d"
             }
           ]} />
           <p className='mt-3'>View more at <Link href={MEDIUM_LINK} target='_blank' style={{
